6e644b85e5000e9e228e8518d220aec451f00328
zknight-droid <84511336+zknight-droid@users.noreply.github.com>
2023-12-21 20:36:08 +0700

    Add files via upload

---
+++ UAS/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.product (sku, brand, "name", description, price, non_discountable) VALUES
+    ('ABCD123', 'ABC Corporation', 'ABC Tea Bags', 'Premium tea bags from ABC Corporation', 15000.0, false),
+    ('XYZ789', 'XYZ Foods', 'XYZ Instant Noodles', 'Delicious instant noodles by XYZ Foods', 5000.0, false),
+    ('MNO456', 'MNO Drinks', 'MNO Sparkling Water', 'Refreshing sparkling water by MNO Drinks', 10000.0, true),
+    ('PQR321', 'PQR Snacks', 'PQR Potato Chips', 'Crunchy potato chips by PQR Snacks', 8000.0, false);
